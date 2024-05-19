239a49bdc205536a39e803f2a64f434c8e69ad11
Kobu <thekobu@gmail.com>
2024-05-19 22:33:03 +0200

    feat: install react query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,26 @@
-import PageTitle from './components/PageTitle'
-import styles from "./styles/modules/app.module.css"
-import AppHeader from './components/AppHeader'
-import AppContent from './components/AppContent'
+import PageTitle from "./components/PageTitle";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import styles from "./styles/modules/app.module.css";
+import AppHeader from "./components/AppHeader";
+import AppContent from "./components/AppContent";
+
+const queryClient = new QueryClient();
 
 function App() {
-
   return (
-    <div className='container'>
-      <PageTitle>TODO List</PageTitle>
-      <div className={styles.app__wrapper}>
-        <AppHeader/>
-        <AppContent/>
+    <QueryClientProvider client={queryClient}>
+      <div className="container">
+        <PageTitle>TODO List</PageTitle>
+        <div className={styles.app__wrapper}>
+          <AppHeader />
+          <AppContent />
+        </div>
       </div>
-    </div>
-  )
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
