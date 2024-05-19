f5794326984e0febcec14549fe74f75e05ecb0a2
Izmi <kcinkcinkcin@seznam.cz>
2024-05-19 21:16:53 +0200

    frontend post task

--- frontend/src/components/TodoModal.tsx
+++ frontend/src/components/TodoModal.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styles from "../styles/modules/modal.module.css";
 import { MdOutlineClose } from "react-icons/md";
 import Button from "./Buttons/Button";
+import { v4 as uuid } from "uuid";
 
 function TodoModal({
   modalOpen,
@@ -10,15 +11,39 @@ function TodoModal({
   modalOpen: boolean;
   setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
-    const [name, setName] = useState("");
-    const [descripton, setDescripton] = useState("");
-    const [deadline, setDeadline] = useState("2024-05-19T12:30");
+  const [name, setName] = useState("");
+  const [description, setDescripton] = useState("");
+  const [date, setDate] = useState("2024-05-19T12:30");
 
-    const hangleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log({name, descripton, deadline});
-        setModalOpen(false);
+  const hangleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log({ name, description, date });
+
+    const activityType = "task"
+    const data = {
+      name,
+      description,
+      date,
+      activityType,
+    };
+    const url = "http://127.0.0.1:5000/create_activity"
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
     }
+    const response = await fetch(url, options);
+    if (response.status !== 201 && response.status !== 200){
+      const message = await response.json()
+      alert(message.message)
+    } else {
+      //sucess
+    }
+
+    setModalOpen(false);
+  };
   return (
     modalOpen && (
       <div className={styles.wrapper}>
@@ -36,22 +61,33 @@ function TodoModal({
             <h1 className={styles.formTitle}>Add task</h1>
             <label htmlFor="name">
               Name
-              <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)}/>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </label>
-            <label htmlFor="descripton">
+            <label htmlFor="description">
               Description
-              <input type="text" id="descripton" value={descripton} onChange={(e) => setDescripton(e.target.value)}/>
+              <input
+                type="text"
+                id="description"
+                value={description}
+                onChange={(e) => setDescripton(e.target.value)}
+              />
             </label>
-            <label htmlFor="deadline">
-              Deadline
+            <label htmlFor="date">
+              date
               <input
                 type="datetime-local"
-                id="deadline"
-                value={deadline} onChange={(e) => setDeadline(e.target.value)}
+                id="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
               />
             </label>
             <div className={styles.buttonContainer}>
-              <Button type="submit" onClick={() =>("")}>
+              <Button type="submit" onClick={() => ""}>
                 Add task
               </Button>
               <Button
