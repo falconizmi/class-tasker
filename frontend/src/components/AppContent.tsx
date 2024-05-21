b3036b7b3299b9da1e4ddb40612ae52c356faafa
Izmi <kcinkcinkcin@seznam.cz>
2024-05-21 23:34:39 +0200

    refactor

--- frontend/src/components/AppContent.tsx
+++ frontend/src/components/AppContent.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import ActivityPage from './pages/ActivityPage'
 
 function AppContent() {
   return (
     <div>
-      this is app content
+      <ActivityPage/>
     </div>
   )
 }
