07aecb42cb7cd90db56977d08354e1463f866b60
Izmi <kcinkcinkcin@seznam.cz>
2024-05-22 00:37:25 +0200

    add query client provied

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 // import './index.css'
-import "./styles/GlobalStyles.css"
-import '@fontsource/poppins'
-import '@fontsource/poppins/500.css'
-import '@fontsource/poppins/600.css'
-import '@fontsource/poppins/700.css'
+import "./styles/GlobalStyles.css";
+import "@fontsource/poppins";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient} >
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
-)
+);
